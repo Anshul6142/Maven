b1d0ec485b0e81faaeae8ceaa650811c7ef212ea
Anshul6142 <2021wc86538@wilp.bits-pilani.ac.in>
2025-07-14 21:15:14 +0530

    change done again

--- src/main/java/com/project/maven_project/MavenProjectApplication.java
+++ src/main/java/com/project/maven_project/MavenProjectApplication.java
@@ -8,7 +8,7 @@ public class MavenProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenProjectApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello World again");
 	}
 
 }
